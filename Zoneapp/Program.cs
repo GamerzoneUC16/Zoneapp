a5da2608cfd93e600a9d21440556c6ac9bf14982
Danyllo Moreira Loureiro <danyllinho2007@hotmail.com>
2023-03-14 09:02:09 -0300

    Adicionar arquivos de projeto.

---
+++ Zoneapp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoneapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
