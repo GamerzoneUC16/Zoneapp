7914f078c99688935ebd786374068b317a9623f1
Kauã Pereira da Silva <kauapereira01@hotmail.com>
2023-03-26 21:09:50 -0300

    nem lembro

--- Zoneapp/FrmChamados.Designer.cs
+++ Zoneapp/FrmChamados.Designer.cs
@@ -49,6 +49,8 @@
             // 
             // dtgLista
             // 
+            this.dtgLista.AllowUserToAddRows = false;
+            this.dtgLista.AllowUserToDeleteRows = false;
             this.dtgLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dtgLista.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.clnId,
@@ -62,11 +64,13 @@
             this.clnCliente,
             this.clnUsuario,
             this.clnDataFinal});
-            this.dtgLista.Location = new System.Drawing.Point(22, 94);
+            this.dtgLista.Location = new System.Drawing.Point(33, 145);
+            this.dtgLista.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dtgLista.Name = "dtgLista";
+            this.dtgLista.ReadOnly = true;
             this.dtgLista.RowHeadersVisible = false;
             this.dtgLista.RowHeadersWidth = 62;
-            this.dtgLista.Size = new System.Drawing.Size(969, 382);
+            this.dtgLista.Size = new System.Drawing.Size(1454, 588);
             this.dtgLista.TabIndex = 1;
             // 
             // clnId
@@ -91,22 +95,28 @@
             // 
             this.clnMotivo.Frozen = true;
             this.clnMotivo.HeaderText = "MOTIVO";
+            this.clnMotivo.MinimumWidth = 8;
             this.clnMotivo.Name = "clnMotivo";
             this.clnMotivo.ReadOnly = true;
+            this.clnMotivo.Width = 150;
             // 
             // clnAssunto
             // 
             this.clnAssunto.Frozen = true;
             this.clnAssunto.HeaderText = "ASSUNTO";
+            this.clnAssunto.MinimumWidth = 8;
             this.clnAssunto.Name = "clnAssunto";
             this.clnAssunto.ReadOnly = true;
+            this.clnAssunto.Width = 150;
             // 
             // clnAnexo
             // 
             this.clnAnexo.Frozen = true;
             this.clnAnexo.HeaderText = "ANEXO";
+            this.clnAnexo.MinimumWidth = 8;
             this.clnAnexo.Name = "clnAnexo";
             this.clnAnexo.ReadOnly = true;
+            this.clnAnexo.Width = 150;
             // 
             // clnStatus
             // 
@@ -133,6 +143,7 @@
             this.clnHashcode.MinimumWidth = 8;
             this.clnHashcode.Name = "clnHashcode";
             this.clnHashcode.ReadOnly = true;
+            this.clnHashcode.Width = 150;
             // 
             // clnCliente
             // 
@@ -141,6 +152,7 @@
             this.clnCliente.MinimumWidth = 8;
             this.clnCliente.Name = "clnCliente";
             this.clnCliente.ReadOnly = true;
+            this.clnCliente.Width = 150;
             // 
             // clnUsuario
             // 
@@ -150,20 +162,24 @@
             this.clnUsuario.Name = "clnUsuario";
             this.clnUsuario.ReadOnly = true;
             this.clnUsuario.ToolTipText = "Não atribuido";
+            this.clnUsuario.Width = 150;
             // 
             // clnDataFinal
             // 
             this.clnDataFinal.Frozen = true;
             this.clnDataFinal.HeaderText = "DATAF";
+            this.clnDataFinal.MinimumWidth = 8;
             this.clnDataFinal.Name = "clnDataFinal";
             this.clnDataFinal.ReadOnly = true;
+            this.clnDataFinal.Width = 150;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(439, 75);
+            this.label1.Location = new System.Drawing.Point(658, 115);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(51, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             // 
@@ -172,22 +188,24 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(378, 36);
+            this.label2.Location = new System.Drawing.Point(567, 55);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(206, 24);
+            this.label2.Size = new System.Drawing.Size(318, 33);
             this.label2.TabIndex = 3;
             this.label2.Text = "LISTA DE CHAMADOS";
             // 
             // FrmChamados
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(1014, 560);
+            this.ClientSize = new System.Drawing.Size(1521, 862);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dtgLista);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmChamados";
             this.Text = "FrmServices";
             this.Load += new System.EventHandler(this.FrmChamados_Load_1);
